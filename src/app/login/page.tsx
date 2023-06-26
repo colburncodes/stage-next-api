ef478611cfaa31be10ca86d75dc028727306dd60
colburncodes <colburnsanders@gmail.com>
2023-06-26 13:25:58 -0500

    feat: :technologist: implemented register page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,72 +1,88 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { LockOutlined, MailOutlined } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Spin } from "antd";
 
 export default function Login() {
+  const [isLoading, setLoading] = useState(false);
+
   const onFinish = (values: any) => {
     console.log("Finish:", values);
   };
 
+  useEffect(() => {
+    setLoading(true);
+  }, []);
+
   return (
-    <div className="flex justify-center h-screen items-center bg-primary">
-      <div className="card p-3 w-450">
-        <h1 className="text-xl">DevPortal - Login</h1>
-        <hr />
-        <Form
-          className="flex flex-col gap-3"
-          name="login"
-          layout="vertical"
-          style={{
-            display: "grid",
-            maxWidth: 600,
-            padding: 20,
-            justifyContent: "center",
-            alignItems: "center",
-            position: "relative",
-          }}
-          initialValues={{ remember: true }}
-          autoComplete="off"
-          onFinish={onFinish}
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
-            style={{
-              paddingBottom: 10,
-              justifyContent: "center",
-              alignItems: "center",
-              width: 400,
-            }}
-          >
-            <Input
-              prefix={<MailOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-            />
-          </Form.Item>
+    <>
+      {!isLoading ? (
+        <Spin size="large" />
+      ) : (
+        <div className="flex justify-center h-screen items-center bg-primary">
+          <div className="card p-3 w-450">
+            <h1 className="text-xl">DevPortal - Login</h1>
+            <hr />
+            <Form
+              className="flex flex-col gap-3"
+              name="login"
+              layout="vertical"
+              style={{
+                display: "grid",
+                maxWidth: 600,
+                padding: 20,
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+              }}
+              initialValues={{ remember: true }}
+              autoComplete="off"
+              onFinish={onFinish}
+            >
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  { required: true, message: "Please input your email!" },
+                ]}
+                style={{
+                  paddingBottom: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: 400,
+                }}
+              >
+                <Input
+                  prefix={<MailOutlined className="site-form-item-icon" />}
+                  placeholder="Email"
+                />
+              </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-            style={{ paddingBottom: 10, width: 400 }}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+                style={{ paddingBottom: 10, width: 400 }}
+              >
+                <Input
+                  prefix={<LockOutlined className="site-form-item-icon" />}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Item>
 
-          <Button type="primary" htmlType="submit" block>
-            Login
-          </Button>
+              <Button type="primary" htmlType="submit" block>
+                Login
+              </Button>
 
-          <Link href="/register">Don't have an account? Register</Link>
-        </Form>
-      </div>
-    </div>
+              <Link href="/register">Don't have an account? Register</Link>
+            </Form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,102 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { Button, Form, Input, Radio } from "antd";
+import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
 
 export default function Register() {
-  return <div>Register Page</div>;
+  const onFinish = (values: any) => {
+    console.log("Finish:", values);
+  };
+
+  return (
+    <div className="flex justify-center h-screen items-center bg-primary">
+      <div className="card p-3 w-450">
+        <h1 className="text-xl">DevPortal - Register</h1>
+        <hr />
+        <Form
+          className="flex flex-col gap-3"
+          name="login"
+          layout="vertical"
+          style={{
+            display: "grid",
+            maxWidth: 600,
+            padding: 20,
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+          }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="Register As"
+            rules={[
+              {
+                required: true,
+                message: "Please register as Employer or Developer",
+              },
+            ]}
+          >
+            <Radio.Group>
+              <Radio value="employer"> Employer </Radio>
+              <Radio value="developer"> Developer </Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name!" }]}
+            style={{
+              paddingBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              width: 400,
+            }}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Name"
+            />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+            style={{
+              paddingBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              width: 400,
+            }}
+          >
+            <Input
+              prefix={<MailOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+            style={{ paddingBottom: 10, width: 400 }}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Button type="primary" htmlType="submit" block>
+            Register
+          </Button>
+
+          <Link href="/login">Already have an account? Login</Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
