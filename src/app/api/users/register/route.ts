32e91d9438ba84901ffa89024b0bd3e047b47a9f
colburncodes <colburnsanders@gmail.com>
2023-06-27 13:51:22 -0500

    feat: :recycle: update test register api

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -1,7 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ConnectDB } from "@/config/db";
+import { User } from "../../../models/user";
+import bcrypt from "bcrypt";
+
 ConnectDB();
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({ message: "users/register POST method accessed!" });
+  try {
+    const request = await req.json();
+    const { name, email, password } = request;
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      throw new Error("User already exist!");
+    }
+
+    return bcrypt.hash(password, 10).then((hash) =>
+      User.create({ name, email, password: hash }).then((user) => {
+        return NextResponse.json(
+          { message: "User created successfully" },
+          { status: 201 }
+        );
+      })
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message, status: 500 });
+  }
 }
