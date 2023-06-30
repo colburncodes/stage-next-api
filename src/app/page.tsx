5acd398bcda6d981d4c52c9bf8a2fdb9d666cff7
colburncodes <colburnsanders@gmail.com>
2023-06-29 21:26:42 -0500

    build: :bug: fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,8 @@
-import axios from "axios";
-import { cookies } from "next/headers";
-
-export async function getUser() {
-  try {
-    const token = cookies().get("token");
-    const response = await axios.get(`${process.env.BASE_URL}api/users/user`, {
-      headers: {
-        Cookie: `token=${token?.value}`,
-      },
-    });
-    return response.data.data;
-  } catch (error: any) {
-    console.log(error);
-  }
-}
-
 export default async function Home() {
-  const user = await getUser();
-
   return (
     <main>
       <h1>Dev Portal</h1>
-      <p>Authorized User {user && user.email}</p>
+      <p>Authorized User </p>
     </main>
   );
 }
