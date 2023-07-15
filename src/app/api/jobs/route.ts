7288e5e85224d809bea1149b3bc4ec054f18251d
colburncodes <colburnsanders@gmail.com>
2023-07-15 09:58:52 -0500

    feat: :sparkles: display listed jobs in table and navigation for edit jobs.

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -4,6 +4,22 @@ import { validateToken } from "@/utility/validateToken";
 import { NextRequest, NextResponse } from "next/server";
 ConnectDB();
 
+export async function GET(req: NextRequest) {
+  try {
+    // add condition later to list all jobs based on the user
+    const jobs = await Job.find();
+    if (!jobs) {
+      throw new Error("No jobs exist!");
+    }
+    return NextResponse.json({
+      message: "Jobs fetched successfully",
+      data: jobs,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const userId = await validateToken(req);
---
+++ src/app/jobs/edit/[id]/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import JobForm from "@/components/JobForm";
+import PageTitle from "@/components/PageTitle";
+import { Button, Form } from "antd";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function Edit() {
+  const router = useRouter();
+
+  const onFinish = () => {};
+  return (
+    <div>
+      <div className="flex justify-between">
+        <PageTitle title={"Edit Job"} />
+        <Button type="default" onClick={() => router.back()}>
+          Back
+        </Button>
+      </div>
+
+      <Form layout="vertical" onFinish={onFinish}>
+        <JobForm />
+        <div className="flex justify-end items-center gap-3 py-2">
+          <Button type="default" onClick={() => router.back()}>
+            Cancel
+          </Button>
+          <Button type="primary" htmlType="submit">
+            Update Job
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
