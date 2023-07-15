9510bb9a78fe1adcbf8ac7c25cae4d670b77d3d7
colburncodes <colburnsanders@gmail.com>
2023-07-15 10:37:50 -0500

    fix: :green_heart: maybe variable interpolation matters

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -2,7 +2,7 @@ import EmailTemplate from "@/components/Email";
 import { Resend } from "resend";
 import { NextRequest, NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(`${process.env.RESEND_API_KEY}`);
 
 export async function POST(req: NextRequest) {
   try {
